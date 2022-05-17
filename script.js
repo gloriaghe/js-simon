ac6a81c419bdc0f601230be5802155f790a30b60
Gloria <gloghe@yahoo.it>
2022-05-17 15:02:52 +0200

    numeri utente

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ const numberHtml = document.querySelector("h2");
 //numeri
 const numberRandom = creaRandomNum(5, 1, 100);
 numberHtml.innerHTML += numberRandom;
+let ArreynumRandom = [];
+ArreynumRandom.push(numberRandom);
+console.log(ArreynumRandom)
 
 //10 secondi durata numeri
 setTimeout(numRandomTime, 10000);
@@ -27,5 +30,21 @@ function numRandomTime() {
     numberHtml.innerHTML = "";
 }
 
+let numUtenteArray = [];
 
+setTimeout(numRandomTimeUser, 20000);
+function numRandomTimeUser() {
+    let numeriUtente = prompt("Inserisci i 5 numeri visti");
 
+    console.log(numeriUtente);
+    numUtenteArray.push(numeriUtente);
+    console.log(numUtenteArray);
+}
+
+debugger;
+let numIndovinati = [];
+if (ArreynumRandom.includes(numUtenteArray)){
+    
+    numIndovinati.push(numUtenteArray);
+    console.log(numIndovinati)
+}
